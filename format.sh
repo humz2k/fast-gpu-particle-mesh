52e82e70b2ab0ad8bb7648623b574246eb7d2798
Humza Qureshi <humzaqureshis@gmail.com>
2024-07-16 18:05:11 -0500

    format

--- format.sh
+++ format.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 clang-format -i src/**.cpp
-clang-format -i src/**.cu
+clang-format -i src/cuda/**.cu
 clang-format -i src/**.hpp
 clang-format -i drivers/**.cpp
 clang-format -i include/**.hpp
--- src/cuda/mpi_distribution.cu
+++ src/cuda/mpi_distribution.cu
@@ -1,20 +1,18 @@
 #include "mpi_distribution.hpp"
 
-MPIDist::MPIDist(int ng) : m_ng(ng), m_local_grid_size{ng,ng,ng}{
+MPIDist::MPIDist(int ng) : m_ng(ng), m_local_grid_size{ng, ng, ng} {}
 
+MPIDist::MPIDist(int ng, int rank, int3 rank_dims, int3 rank_coords,
+                 int3 local_grid_size)
+    : m_ng(ng), m_rank(rank), m_rank_dims(rank_dims),
+      m_rank_coords(rank_coords), m_local_grid_size(local_grid_size) {}
+
+__host__ __device__ int3 MPIDist::idx2local(int idx) {
+    return make_int3((idx / m_local_grid_size.z) / m_local_grid_size.y,
+                     (idx / m_local_grid_size.z) % m_local_grid_size.y,
+                     idx % m_local_grid_size.z);
 }
 
-MPIDist::MPIDist(int ng, int rank, int3 rank_dims, int3 rank_coords, int3 local_grid_size) : m_ng(ng), m_rank(rank), m_rank_dims(rank_dims), m_rank_coords(rank_coords), m_local_grid_size(local_grid_size){
-
-}
-
-__host__ __device__ int3 MPIDist::idx2local(int idx){
-    return make_int3(
-        (idx/m_local_grid_size.z)/m_local_grid_size.y,
-        (idx/m_local_grid_size.z)%m_local_grid_size.y,
-        idx%m_local_grid_size.z);
-}
-
-__host__ __device__ int3 MPIDist::idx2global(int idx){
+__host__ __device__ int3 MPIDist::idx2global(int idx) {
     return idx2local(idx) + (m_rank_coords * m_local_grid_size);
 }
