e20dd69597622151741d05f0e428ee9f0e9983af
Humza Qureshi <humzaqureshis@gmail.com>
2024-08-12 16:27:40 -0500

    testing initial conditions

--- drivers/testic.cpp
+++ drivers/testic.cpp
@@ -18,45 +18,28 @@ class SineGrid : public SimpleGrid<fft_wrapper_t, fft_t> {
 
         this->forward();
 
+        launch_scale_amplitudes_by_power_spectrum(this->grid(), cosmo.initial_pk(),
+                                                  this->params().rl(), this->dist(),
+                                                  numBlocks, blockSize);
+
         fft_t* h_grid;
         cpu_allocator.alloc(&h_grid, this->size() * sizeof(fft_t));
         gpuMemcpy(h_grid, this->grid(), this->size() * sizeof(fft_t),
                   gpuMemcpyDeviceToHost);
 
-        // float ng = this->dist().ng();
-
         for (size_t i = 0; i < this->size(); i++) {
-            float sk = 0;
-            // if (i == 10){
-            //     sk = 1;
-            // }
-
-            float3 kmodes =
-                this->dist().kmodes(i, (2.0f * M_PI) / this->params().rl());
-            float k = len(kmodes);
-
-            // if (k == 1){
-            //     sk = 1;
-            // }
-            float center = 5;
-            float delta = 0.1;
-
-            if ((k <= (center + delta)) && (k >= (center - delta))) {
-                sk = 1;
+            if (i == 100){
+                auto kmodes = this->dist().kmodes(i,2*M_PI/((float)this->params().ng()));
+                printf("setting k %g %g %g (%g) to not 0\n",kmodes.x,kmodes.y,kmodes.z,len(kmodes));
+                continue;
             }
-            h_grid[i].x *= sk;
-            h_grid[i].y *= sk;
+            h_grid[i].x = 0;
+            h_grid[i].y = 0;
         }
 
         gpuMemcpy(this->grid(), h_grid, this->size() * sizeof(fft_t),
                   gpuMemcpyHostToDevice);
         cpu_allocator.free(h_grid);
-
-        // launch_scale_amplitudes_by_power_spectrum(this->grid(),
-        // cosmo.initial_pk(),
-        //                                         this->params().rl(),
-        //                                         this->dist(), numBlocks,
-        //                                         blockSize);
     }
 };
 
@@ -64,12 +47,6 @@ class SineParticles : public SerialParticles {
   public:
     SineParticles(const Params& params, Cosmo& cosmo, Timestepper& ts)
         : SerialParticles(params, cosmo, ts) {
-        /**
-         * SerialParticles(...) will allocate the correct amount of space in
-         * this->pos() and this->vel(); but will not initialize particles in the
-         * arrays. So, we need to call generate_displacement_ic on some grid in
-         * order to generate the initial conditions.
-         */
         SineGrid<SerialFFT, complexDoubleDevice> ic_grid(this->params(),
                                                          this->params().np());
         ic_grid.generate_displacement_ic(cosmo, ts, *this);
@@ -90,8 +67,8 @@ int main() {
     events.timers["dinit"].end();
 
     SimpleGrid<SerialFFT, complexFloatDevice> grid(params, params.ng());
-    PowerSpectrum ic_power(particles, grid, params.pk_n_bins(), 1);
-    ic_power.to_csv(params.output_prefix() + "init_power_spectrum.csv");
+    PowerSpectrum(particles, grid, params.pk_n_bins(), 0).to_csv(params.output_prefix() + "fold0.csv");
+    PowerSpectrum(particles, grid, params.pk_n_bins(), 1).to_csv(params.output_prefix() + "fold1.csv");
 
     MPI_Finalize();
     return 0;
